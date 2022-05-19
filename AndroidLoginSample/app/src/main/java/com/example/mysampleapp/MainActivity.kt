dc2b5892d90aec12e1a94ac26fb556563a465aec
LeeWang <kiwan1111@gmail.com>
2022-05-20 03:38:15 +0900

    Step.1 commit

--- AndroidLoginSample/app/src/main/java/com/example/mysampleapp/MainActivity.kt
+++ AndroidLoginSample/app/src/main/java/com/example/mysampleapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.mysampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = Firebase.auth
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val joinBtnClicked = findViewById<Button>(R.id.joinBtn)
+        joinBtnClicked.setOnClickListener{
+            auth.createUserWithEmailAndPassword("abs@abc.anc", "12341234")
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this,"ok",Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this,"no",Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+        }
     }
 }
